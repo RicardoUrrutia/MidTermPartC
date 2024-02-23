54ae332fe6537bf5bc094049cfb02cfedf134d8a
RicardoUrrutia <villaltr@sheridancollege.ca>
2024-02-23 16:41:53 -0500

    Added Ricardo_Design

---
+++ src/Ricardo_Design/java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ricardo_Design;
+
+/**
+ *
+ * @author ricky
+ */
+public class java {
+    /*
+    Im not sure what we are supposed to commment in here but a topic I like is computers. I enjoy builing computers. 
+    I build my pc myself. My CPU is a Ryzen 7 7800X3D and my GPU is 4080 super.
+    */
+}
